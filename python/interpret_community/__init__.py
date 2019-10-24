6a169ab40479f76c7ae6346a40a897eed8ca0055
eedeleon <31962564+eedeleon@users.noreply.github.com>
2019-10-24 15:12:19 -0400

    Add handler to interpret to catch warnings

--- python/interpret_community/__init__.py
+++ python/interpret_community/__init__.py
@@ -15,8 +15,17 @@ __all__ = ["TabularExplainer"]
 # Setup logging infrustructure
 import logging
 import os
+
+# Add handler to not drop warnings from interpret
+interpret_logger = logging.getLogger("interpret")
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARNING)
+interpret_logger.addHandler(stream_handler)
+
 # Only log to disk if environment variable specified
 interpretc_logs = os.environ.get('INTERPRETC_LOGS')
 if interpretc_logs is not None:
     logging.basicConfig(filename=interpretc_logs, level=logging.INFO)
     logging.info('Initializing logging file for interpret-community')
+    
+   

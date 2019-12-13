0df0d87a514459fcc4d8dea6e8aa89500f68f1aa
Eduardo de Leon <eddeleon@microsoft.com>
2019-12-13 11:48:58 -0500

    Save working e2e for ShapKernel + interpret_community

--- python/interpret_community/processing.py
+++ python/interpret_community/processing.py
@@ -1,47 +1,65 @@
 
 PREPROCESSOR_KEY = "preprocessors"
-POSTPROCESSOR_KEY = "postprocessors"
+POSTPROCESSORS_KEY = "postprocessors"
 
 
 def add_preprocessing(base_class):
     base_class_init = base_class.__init__
 
     def new_init(self, func, data, *args, **kwargs):
-        if PREPROCESSOR_KEY in kwargs and kwargs.get(PREPROCESSOR_KEY):
-            for preprocessor in kwargs.get(PREPROCESSOR_KEY):
+        self._preprocessors = kwargs.pop(PREPROCESSOR_KEY, None)
+        if self._preprocessors:
+            for preprocessor in self._preprocessors:
                 data = preprocessor(data)
-            base_class_init(self, func, data, *args, **kwargs)
-        base_class.__init__ = new_init
+        base_class_init(self, func, data, *args, **kwargs)
+
+    if hasattr(base_class, "explain_local"):
+        if not hasattr(base_class, "_explain_local"):
+            base_class._explain_local = base_class.explain_local
+
+        def new_explain_local(self, data, *args, **kwargs):
+            import pdb; pdb.set_trace()
+            if self._preprocessors:
+                for preprocessor in self._preprocessors:
+                    data = preprocessor(data)
+            import pdb; pdb.set_trace()
+            return base_class._explain_local(self, data, *args, **kwargs)
+        base_class.explain_local = new_explain_local
+
+    base_class.__init__ = new_init
     return base_class
 
 
 def add_postprocessing(base_class):
     base_class_init = base_class.__init__
 
-    def new_init(self, *args, **kwargs):
-        if POSTPROCESSOR_KEY in kwargs and kwargs.get(POSTPROCESSOR_KEY):
-            self._internal_postprocessor = kwargs.get(POSTPROCESSOR_KEY)
-            base_class_init(self, *args, **kwargs)
-    base_class.__init__ = new_init
+    def new_init(self, func, data, *args, **kwargs):
+        self._postprocessors = kwargs.pop(POSTPROCESSORS_KEY, None)
+        base_class_init(self, func, data, *args, **kwargs)
 
     if hasattr(base_class, "explain_local"):
         base_class._original_explain_local = base_class.explain_local
 
         def new_explain_local(self, *args, **kwargs):
+            post_processors = self._postprocessors
             explanation = base_class._original_explain_local(self, *args, **kwargs)
-            if hasattr(self, "_internal_postprocessor"):
-                for postprocessor in self._internal_postprocessors:
+            if post_processors:
+                for postprocessor in post_processors:
                     explanation = postprocessor(explanation)
             return explanation
-    base_class.explain_local = new_explain_local
+        base_class.explain_local = new_explain_local
 
     if hasattr(base_class, "explain_global"):
+        base_class._original_explain_global = base_class.explain_global
+
         def new_explain_global(self, *args, **kwargs):
+            post_processors = self._postprocessors
             explanation = base_class._original_explain_global(self, *args, **kwargs)
-            if hasattr(self, "_internal_postprocessor"):
-                for postprocessor in self._internal_postprocessors:
+            if post_processors:
+                for postprocessor in post_processors:
                     explanation = postprocessor(explanation)
             return explanation
-    base_class.explain_global = new_explain_global
+        base_class.explain_global = new_explain_global
 
+    base_class.__init__ = new_init
     return base_class
